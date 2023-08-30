217860abb46aa3612dd15af92811f12655a7657d
gala <andhoumani13003.g@gmail.com>
2023-08-30 14:18:34 +0200

    ajout du css composent CardList

--- frontend/src/components/CardList.jsx
+++ frontend/src/components/CardList.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
+import "./CardList.css"
 
 export default function CardList() {
   const [products, setProducts] = useState([])
@@ -12,13 +13,19 @@ export default function CardList() {
   }, [])
 
   return (
-    <header className="App-header">
-      {products.map((product) => (
-        <div key={product.id}>
-          <img src={product.image} alt={product.name} />
-          <p key={product.id}>{product.name}</p>
-        </div>
-      ))}
-    </header>
+    <div className="card-list-container">
+      <div className="card-group">
+        {products.map((product, index) => (
+          <div className="card" key={product.id}>
+            <img
+              className="card-image"
+              src={product.image}
+              alt={product.name}
+            />
+            <p className="card-name">{product.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }

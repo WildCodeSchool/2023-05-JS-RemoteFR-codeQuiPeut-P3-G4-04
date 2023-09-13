6c91eb173a034bc05c0230ea04ffe133c40f7612
gala <andhoumani13003.g@gmail.com>
2023-09-13 12:56:54 +0200

    ajout

--- frontend/src/pages/Commandes.jsx
+++ frontend/src/pages/Commandes.jsx
@@ -1,12 +1,9 @@
-/* eslint-disable no-undef */
-/* eslint-disable react/jsx-no-undef */
 import "./Commandes.css"
 
 function Commandes() {
   return (
     <div>
       <h1>Commandes</h1>
-      <CartSummary cart={cart} />
     </div>
   )
 }
